91a130e9d0e6a7ce46f7e5c9f58e7d3686c84b33
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-22 11:10:00 +0300

    Initial Commit

---
+++ Dicee-SwiftUI-Section-17/Dicee_SwiftUI_Section_17App.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUI_Section_17App.swift
+//  Dicee-SwiftUI-Section-17
+//
+//  Created by Abdurahman on 22.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUI_Section_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

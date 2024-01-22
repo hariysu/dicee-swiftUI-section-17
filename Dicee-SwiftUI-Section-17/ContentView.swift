46785d239b85ef6efcee5ca62ed788e14a342a50
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-22 12:01:14 +0300

    211-spacer,button

--- Dicee-SwiftUI-Section-17/ContentView.swift
+++ Dicee-SwiftUI-Section-17/ContentView.swift
@@ -9,18 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(number: "dice1")
+                    DiceView(number: "dice6")
+                }.padding(.horizontal)
+                Spacer()
+                Button(action: {}) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }.background(Color.red)
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    let number: String
+    
+    var body: some View {
+        Image(number)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding(.all)
     }
 }
